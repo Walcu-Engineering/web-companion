1c727416706ee855031ca9c9fc93ad4722cae40d
David Arroyo <davidarroyo25052004@gmail.com>
2026-04-09 10:44:16 +0200

    feat add calculator SDK and demo UI

---
+++ backend/public/calculator.js
@@ -0,0 +1,15 @@
+(function(window) {
+  const calculator = {
+    add(a, b) {
+      return a + b
+    },
+    substract(a, b) {
+      return a - b
+    },
+    multiply(a, b) {
+      return a * b
+    }
+  }
+
+  window.calculator = calculator
+})(window)
