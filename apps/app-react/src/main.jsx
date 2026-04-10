83e91107090efd6efb4eddf90ca3787890e358ac
David Arroyo <davidarroyo25052004@gmail.com>
2026-04-10 12:10:52 +0200

    refactor: rename the sdk on all clones

--- apps/app-react/src/main.jsx
+++ apps/app-react/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '../../../packages/sdk-ui/widget-sdk.js'
+import '../../../packages/sdk-ui/CallWidgetSDK.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- apps/app-vue/src/main.js
+++ apps/app-vue/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-import '../../../packages/sdk-ui/widget-sdk.js'
+import '../../../packages/sdk-ui/CallWidgetSDK.js'
 
 const app = createApp(App)
 app.use(router)
